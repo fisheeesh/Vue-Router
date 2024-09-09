39b189f01586a5b17fb4a251a2098e0bdb180b50
fisheeesh <swanphyo444@gmail.com>
2024-09-09 15:58:09 +0700

    advantage of router-link, create a route and file structure

---
+++ src/views/HomeView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">
+    <h1>This is an home page</h1>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis rerum sit ab architecto molestias et impedit autem
+      recusandae, iure voluptas eveniet aliquid aut itaque incidunt, tenetur non. Eaque, blanditiis reiciendis?</p>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis rerum sit ab architecto molestias et impedit autem
+        recusandae, iure voluptas eveniet aliquid aut itaque incidunt, tenetur non. Eaque, blanditiis reiciendis?</p>
+  </div>
+</template>
+
+<script>
+
+export default {
+
+}
+</script>
---
+++ src/views/jobs/JobsView.vue
@@ -0,0 +1,27 @@
+<template>
+  <h1>Jobs Title</h1>
+  <div v-for="job in jobs" :key="job.id" class="jobs">
+    <h3>{{ job.title }}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            jobs: [
+                {id: 1, title: "Project Manger", detail : "Lead project"},
+                {id: 2, title: "UI/UX Designer", detail : "Design UI/UX"},
+                {id: 3, title: "Frontend Developer", detail : "Develop Frontend"},
+                {id: 4, title: "Backend Developer", detail : "Develop Backend"},
+            ]
+        }
+    }
+}
+</script>
+
+<style>
+.jobs{
+    color: crimson;
+}
+</style>
